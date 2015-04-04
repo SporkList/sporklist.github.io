d2951b9be7c68c169ed6ed26cb33e52393db295e
Yiwen Song <ys@berkeley.edu>
2015-04-04 13:59:01 -0700

    please work

--- scripts/mainwindow.js
+++ scripts/mainwindow.js
@@ -3,26 +3,24 @@
 console.log("saldjflsdjfklsjdlf");
 
 /* This is the angular for the main window */
-function () {
-  angular.module('MainWindow',[]).controller('MainCtrl',function($scope) {
-    /* EDIT THE FOLLOWING FOR REAL DATA */
+angular.module('MainWindow',[]).controller('MainCtrl',function($scope) {
+  /* EDIT THE FOLLOWING FOR REAL DATA */
 
-    console.log("b");
+  console.log("b");
 
-    /* The defaults for the home page.
-     * The only thing that should show is the sidebar and the fb login.
-     */
-    $scope.user = false;
-    $scope.friend = false;
-    $scope.search = false;
-    $scope.map = false;
+  /* The defaults for the home page.
+   * The only thing that should show is the sidebar and the fb login.
+   */
+  $scope.user = false;
+  $scope.friend = false;
+  $scope.search = false;
+  $scope.map = false;
 
-    console.log($scope.user);
-    console.log($scope.user);
-    console.log($scope.user);
-    console.log($scope.user);
-    console.log($scope.user);
+  console.log($scope.user);
+  console.log($scope.user);
+  console.log($scope.user);
+  console.log($scope.user);
+  console.log($scope.user);
 
 
-  });
-}();
+});
