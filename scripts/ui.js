a13fd8d2c49ade547c61f753547f1fadab1ca5c0
Yiwen Song <ys@berkeley.edu>
2015-04-04 02:59:04 -0700

    Angular does not work. pls help.

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,27 +1,45 @@
-/* This is the controller that will control how the search rsults loook */
+/* This is the controller that will control how the search rsults loook 
 var srcInfo = angular.module('SrcInfo', []);
 
 yelpInfo.controller('SrcInfoCtrl',function () {
   
 });
+*/
 
-/* This is the info for the profile of you and your friends */
+/* This is the info for the profile of you and your friends 
 var friendInfo = angular.module('FriendInfo',[]);
 
 friendInfo.controller('FriendInfoCtrl', function () {
 
 });
+*/
 
-/* This is what's in your playlist */
+/* This is what's in your playlist 
 var playlist = angular.module('Playlist', []);
 
 playlist.controller('PlaylistCtrl', function () {
 
 });
+*/
 
-/* This is your list of playlists */
+/* This is your list of playlists
 var playlistList = angular.module('PlaylistList',[]);
 
 playlistLIst.controller('PLLCtrl', function() {
 
 });
+*/
+
+/* This is the wrapper for the entire UI */
+(function () {
+  angular.module('UIWrapper',[]).controller('UICtrl',function($scope) {
+    /* Debug code */
+    console.log("ablsdjflasjdlf");
+    /* playlists -> get playlists from Parse */
+    $scope.playlists = [
+      {'name':'abc'},
+      {'name':'def'},
+      {'name':'xyz'}
+    ];
+  });
+}());
