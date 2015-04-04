f49e548d13277868cf010fe1c1ebb27d87dbe0a5
Alex Kang <akang95@gmail.com>
2015-04-04 14:30:18 -0700

    fuck

--- scripts/mainwindow.js
+++ scripts/mainwindow.js
@@ -1,11 +1,10 @@
 /* Hides 'my-profile-box by default */
 
-console.log("saldjflsdjfklsjdlf");
-
 /* This is the angular for the main window */
 (function () {
   angular.module('MainWindow',[]).controller('MainCtrl',function($scope) {
     /* EDIT THE FOLLOWING FOR REAL DATA */
+    console.log("yessss");
 
     /* The defaults for the home page.
      * The only thing that should show is the sidebar and the fb login.
