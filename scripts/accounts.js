cc745ffbca0ec92337b736f2d0bf6b54c5ecb386
Alex Kang <akang95@gmail.com>
2015-04-04 09:44:28 -0700

    reverting changes

--- scripts/accounts.js
+++ scripts/accounts.js
@@ -23,11 +23,6 @@ $(document).ready(function() {
                 alert("Error: " + error.code + " " + error.message);
             }
         });
-
-
-        // navigator.geolocation.getCurrentPosition(function(position) {
-
-        // });
     }
 
     Parse.initialize("VXRx5pZQAr263FPLmgqY2FHEa66zEOLIuK3I2rl6", "OQkMhfc7hMHcBBkiUoClnxAfrF8gpmKaC3jNKq5V");
