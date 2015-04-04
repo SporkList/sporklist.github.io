e711469302aa05b5e6437614651344e1bf907a10
Yiwen Song <ys@berkeley.edu>
2015-04-04 07:26:06 -0700

    code cleanup

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,40 +1,6 @@
-/* This is the controller that will control how the search rsults loook 
-var srcInfo = angular.module('SrcInfo', []);
-
-yelpInfo.controller('SrcInfoCtrl',function () {
-  
-});
-*/
-
-/* This is the info for the profile of you and your friends 
-var friendInfo = angular.module('FriendInfo',[]);
-
-friendInfo.controller('FriendInfoCtrl', function () {
-
-});
-*/
-
-/* This is what's in your playlist 
-var playlist = angular.module('Playlist', []);
-
-playlist.controller('PlaylistCtrl', function () {
-
-});
-*/
-
-/* This is your list of playlists
-var playlistList = angular.module('PlaylistList',[]);
-
-playlistLIst.controller('PLLCtrl', function() {
-
-});
-*/
-
-var playlists = ['abc', 'def', 'xyz'];
-
 function updateUserPage(user) {
   /* Do something with the user */
-  playlists = ['abc', 'def', 'xyz', 'fuck you michael'];
+  var playlists = ['abc', 'def', 'xyz', 'fuck you michael'];
 
   var list = document.getElementById("playlists");
   var scope = angular.element(list).scope();
