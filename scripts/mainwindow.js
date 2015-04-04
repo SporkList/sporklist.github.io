d31c231edf4fb5500ce5eca65eb3a8efd923858e
Yiwen Song <ys@berkeley.edu>
2015-04-04 14:05:15 -0700

    please work

--- scripts/mainwindow.js
+++ scripts/mainwindow.js
@@ -1,30 +1,26 @@
 /* Hides 'my-profile-box by default */
 
-console.log("ddd");
+console.log("saldjflsdjfklsjdlf");
 
 /* This is the angular for the main window */
-var mod = angular.module('MainWindow',[]);
+(function () {
+  angular.module('MainWindow',[]).controller('MainCtrl',function($scope) {
+    /* EDIT THE FOLLOWING FOR REAL DATA */
 
-function setScope($scope) {
-  /* EDIT THE FOLLOWING FOR REAL DATA */
+    /* The defaults for the home page.
+     * The only thing that should show is the sidebar and the fb login.
+     */
+    $scope.user = false;
+    $scope.friend = false;
+    $scope.search = false;
+    $scope.map = false;
 
-  console.log("b");
-
-  /* The defaults for the home page.
-   * The only thing that should show is the sidebar and the fb login.
-   */
-  $scope.user = false;
-  $scope.friend = false;
-  $scope.search = false;
-  $scope.map = false;
-
-  console.log($scope.user);
-  console.log($scope.user);
-  console.log($scope.user);
-  console.log($scope.user);
-  console.log($scope.user);
+    console.log($scope.user);
+    console.log($scope.user);
+    console.log($scope.user);
+    console.log($scope.user);
+    console.log($scope.user);
 
 
-}
-
-mod.controller('MainCtrl',setScope($scope))();
+  });
+}());
