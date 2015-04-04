60ca431bb52bd9e5763242c117ca6de03fc74a48
Yiwen Song <ys@berkeley.edu>
2015-04-04 01:53:42 -0700

    scripts skeleton added

---
+++ scripts/ui.js
@@ -0,0 +1,27 @@
+/* This is the controller that will control how the search rsults loook */
+var srcInfo = angular.module('SrcInfo', []);
+
+yelpInfo.controller('SrcInfoCtrl',function () {
+  
+});
+
+/* This is the info for the profile of you and your friends */
+var friendInfo = angular.module('FriendInfo',[]);
+
+friendInfo.controller('FriendInfoCtrl', function () {
+
+});
+
+/* This is what's in your playlist */
+var playlist = angular.module('Playlist', []);
+
+playlist.controller('PlaylistCtrl', function () {
+
+});
+
+/* This is your list of playlists */
+var playlistList = angular.module('PlaylistList',[]);
+
+playlistLIst.controller('PLLCtrl', function() {
+
+});
