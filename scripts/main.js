abd863b424f6b66f802d3509e63196de28d8c9c1
Michael Anthony Knyszek <mknyszek@berkeley.edu>
2015-04-04 02:40:39 -0700

    playlist pane layout ready

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,16 @@
 function setPlaylistHeight() {
-    $("#playlists").css('height', window.innerHeight - 60 - 144 - 2);
+    var sum = 0;
+    $("#playlists-pane").children().each(function() {
+        $this = $(this);
+        if($this.attr('id') != "playlists") {
+            sum += $this.height();
+            sum += parseInt($this.css("borderTopWidth"));
+            sum += parseInt($this.css("borderBottomWidth"));
+            sum += parseInt($this.css("paddingTop"));
+            sum += parseInt($this.css("paddingBottom"));
+        }
+    });
+    $("#playlists").css('height', window.innerHeight - sum);
 }
 
 $(document).ready(function () {
