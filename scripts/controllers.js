37e0dc6822a49e30337029973bbff6de59ccf619
Alex Kang <akang95@gmail.com>
2015-04-05 10:44:37 -0700

    adsfawef

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -11,13 +11,6 @@
       var restaurant = Parse.Object.extend("Restaurant");
       var query = new Parse.Query(restaurant);
 
-      var main = document.getElementById("info-pane");
-      var scope = angular.element(main).scope();
-
-      scope.$apply(function() {
-          scope.isMe = "true";  
-      });
-
       query.equalTo("sporklists", playlist.id);
       query.limit(1000);
       query.withinMiles("location", new Parse.GeoPoint(position.latitude, position.longitude), 50);
@@ -118,7 +111,6 @@
     $scope.sporklist = false;
     $scope.search = false;
     $scope.map = false;
-    $scope.isMe = true;
   });
 
   app.controller('MeCtrl', function($scope) {
@@ -137,13 +129,6 @@
       var restaurant = Parse.Object.extend("Restaurant");
       var query = new Parse.Query(restaurant);
 
-      var main = document.getElementById("info-pane");
-      var scope = angular.element(main).scope();
-
-      scope.$apply(function() {
-          scope.isMe = "false";  
-      });
-
       query.equalTo("sporklists", sporklist.id);
       query.limit(1000);
       query.withinMiles("location", new Parse.GeoPoint(position.latitude, position.longitude), 50);
@@ -161,6 +146,7 @@
   app.controller('sporkCtrl', function($scope) {
     $scope.name = "";
     $scope.restaurants = [];
+    $scope.isMe = true;
 
     $scope.follow = function (obj) {
       window.open(obj.get('url'));
