d9f65e86a1a03b8d3bb80907eba74981e49b972f
Alex Kang <akang95@gmail.com>
2015-04-05 02:50:27 -0700

    GAAHHH

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+	console.log("1");
 
 	$(".playlist-wrapper").mouseenter(function() {
+		console.log("2");
 		$($(this).children()[0]).css("visibility", "visible");
 	});
 
