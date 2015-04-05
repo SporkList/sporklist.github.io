9c02a06ef868ec4db5cae17913c8a575d7604f9b
Michael Anthony Knyszek <mknyszek@berkeley.edu>
2015-04-05 10:13:59 -0700

    more attempts

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,7 +1,9 @@
 function onMouseEnter(e) {
-    this.firstChild.style.visibility='visible';
+    //this.firstChild.style.visibility='visible';
+    $(this).children('.delete').show();
 }
 
 function onMouseLeave(e) {
-    this.firstChild.style.visibility='hidden';
+    //this.firstChild.style.visibility='hidden';
+    $(this).children('.delete').hide();
 }
